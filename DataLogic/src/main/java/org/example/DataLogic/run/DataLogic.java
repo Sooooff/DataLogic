b85fc25147a903b982a686520ca953b34b9f95d2
Sofia <165695705+Sooooff@users.noreply.github.com>
2025-11-24 20:05:59 -0600

    Conección a la base de datos de Postgrees

--- DataLogic/src/main/java/org/example/DataLogic/run/DataLogic.java
+++ DataLogic/src/main/java/org/example/DataLogic/run/DataLogic.java
@@ -11,7 +11,7 @@ import org.openxava.util.*;
 public class DataLogic {
 
 	public static void main(String[] args) throws Exception {
-		DBServer.start("DataLogic-db"); // To use your own database comment this line and configure src/main/webapp/META-INF/context.xml
+		//DBServer.start("DataLogic-db"); // To use your own database comment this line and configure src/main/webapp/META-INF/context.xml
 		AppServer.run("DataLogic"); // Use AppServer.run("") to run in root context
 	}
 
